f8316e0ee9e06da4f19947632924556d0277dd06
5hyun <86971770+5hyun@users.noreply.github.com>
2022-05-20 20:59:49 +0900

    필수 조건이랑 배경 사진만 구현

---
+++ 노마드코더_졸업작품/js/backgroundImg.js
@@ -0,0 +1,10 @@
+const images = ["짱구.png", "짱구2.png", "스누피.png"];
+
+function setImage() {
+  const chosenImage = images[Math.floor(Math.random() * images.length)];
+  const bgImage = document.createElement("img");
+  bgImage.src = `img/${chosenImage}`;
+  document.body.appendChild(bgImage);
+}
+
+setImage();
